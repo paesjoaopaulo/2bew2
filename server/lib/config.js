b00a818d4a6247e22e5356b4547564d1a95e7036
João Paulo Paes <joaopaulopaez@gmail.com>
2018-12-07 18:11:59 -0200

    DB

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    //uri: 'mongodb://daguano:daguano01@ds039950.mlab.com:39950/2bew2',
-    uri: 'mongodb://localhost:27017/2bew2',
-    db: '2bew2',
+    uri: 'mongodb://daguano:daguano01@ds039950.mlab.com:39950/dagupaes',
+    //uri: 'mongodb://localhost:27017/2bew2',
+    db: 'dagupaes',
     options: {
         useNewUrlParser: true
     }
 };
+
